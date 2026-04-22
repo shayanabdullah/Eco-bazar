c627d5c2ae665f1518e1034984f99fb25c8354f2
shayanabdullah <tuhinafroza07@gmail.com>
2026-04-23 01:22:23 +0600

    page and package setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
-
+import {Routes, Route } from "react-router";
+import Home from './pages/Home';
 const App = () => {
   return (
-    <div>hello</div>
+   <>
+       <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+   </>
   )
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+
+const Home = () => {
+  
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home
