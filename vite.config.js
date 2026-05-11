248c06b2fbe497715ccdc76b7058b5b382a9aa8a
shayanabdullah <tuhinafroza07@gmail.com>
2026-05-11 22:47:46 +0600

    add PWA

--- vite.config.js
+++ vite.config.js
@@ -1,35 +1,44 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import { VitePWA } from 'vite-plugin-pwa'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),
-     VitePWA({
-      registerType: 'autoUpdate',
+  plugins: [
+    react(),
+    tailwindcss(),
+VitePWA({
+  registerType: 'autoUpdate',
 
-      manifest: {
-        name: 'Eco Bazar',
-        short_name: 'EcoBazar',
-        description: 'Modern Grocery E-commerce Web App',
-        theme_color: '#22c55e',
-        background_color: '#ffffff',
-        display: 'standalone',
+  devOptions: {
+    enabled: true
+  },
 
-        icons: [
-          {
-            src: '/logo.webp',
-            sizes: '192x192',
-            type: 'image/webp'
-          },
-          {
-            src: '/icon-512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
+  includeAssets: ['icon-192.png', 'icon-512.png'],
+
+  manifest: {
+    name: 'Eco Bazar',
+    short_name: 'EcoBazar',
+    description: 'Modern Grocery E-commerce Web App',
+    theme_color: '#22c55e',
+    background_color: '#ffffff',
+    display: 'standalone',
+    start_url: '/',
+
+    icons: [
+      {
+        src: '/icon-192.png',
+        sizes: '192x192',
+        type: 'image/png'
+      },
+      {
+        src: '/icon-512.png',
+        sizes: '512x512',
+        type: 'image/png'
       }
-    })
-  ],
+    ]
+  }
 })
+  ],
+});
